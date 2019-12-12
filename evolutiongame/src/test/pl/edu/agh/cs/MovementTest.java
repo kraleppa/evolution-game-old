e4c87553df42c28c2da6b2cc721614f00335ad09
Krzysztof Nalepa <misiek.nalepa@wp.pl>
2019-12-12 16:27:52 +0100

    Tests updated

--- evolutiongame/src/test/pl/edu/agh/cs/MovementTest.java
+++ evolutiongame/src/test/pl/edu/agh/cs/MovementTest.java
@@ -3,6 +3,8 @@ package pl.edu.agh.cs;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Vector;
+
 
 public class MovementTest {
     @Test
@@ -33,6 +35,45 @@ public class MovementTest {
         Assert.assertEquals(rocky, map.objectAt(new Vector2D(5,6)));
         rocky.move();
         Assert.assertEquals(rocky, map.objectAt(new Vector2D(5,0)));
+
+        Animal fox = new Animal(map, new Vector2D(5, 0));
+        map.place(fox);
+        fox.turnAround(2);
+        fox.move();
+        Assert.assertEquals(fox, map.objectAt(new Vector2D(6, 0)));
+        fox.move();
+        Assert.assertEquals(fox, map.objectAt(new Vector2D(0,0)));
+
+        Animal bison = new Animal(map, new Vector2D(0,0));
+        map.place(bison);
+        bison.turnAround(5);
+        bison.move();
+
+        Assert.assertEquals(bison, map.objectAt(new Vector2D(6,6)));
+        //0,6
+
+        WorldMap map2 = new WorldMap(new Vector2D(6,6));
+
+        Animal horse = new Animal(map2, new Vector2D(6,6));
+        map2.place(horse);
+        horse.turnAround(1);
+        horse.move();
+        Assert.assertEquals(horse, map2.objectAt(new Vector2D(0,0)));
+
+        Animal pig = new Animal(map2, new Vector2D(6,0));
+        map2.place(pig);
+        pig.turnAround(3);
+        pig.move();
+        Assert.assertEquals(pig, map2.objectAt(new Vector2D(0,6)));
+
+        WorldMap map3 = new WorldMap(new Vector2D(6,6));
+        Animal sheep =  new Animal(map3, new Vector2D(0,6));
+        map3.place(sheep);
+        sheep.turnAround(7);
+        sheep.move();
+        Assert.assertEquals(sheep, map3.objectAt(new Vector2D(6,0)));
+
+
     }
 
 }
